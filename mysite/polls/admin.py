dc7b488917e6f4be6967e8c482955292bf270881
Aahil Ashiq Ali <aahilashiqali@gmail.com>
2025-05-01 18:15:07 +0500

    Corrected Order

--- mysite/polls/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from polls.models import Contact, EliteEstateRoyce
-
-# Register your models here.
-admin.site.register(Contact)
-admin.site.register(EliteEstateRoyce)
